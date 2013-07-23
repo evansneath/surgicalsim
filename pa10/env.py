c64cb10da462cea9c45e220126a09ab15e2f0c1f
Evan Sneath <evansneath@gmail.com>
2013-07-23 13:00:44 -0400

    Moved around joint limits to tasks.py and modified power calculations

--- pa10/env.py
+++ pa10/env.py
@@ -7,116 +7,44 @@ Code modified from PyBrain CCRLGlas example model.
 """
 
 from pybrain.rl.environments.ode import ODEEnvironment, sensors, actuators
-from scipy import array, pi
+
+import numpy as np
 
 
 class Pa10Environment(ODEEnvironment):
-    def __init__(self, xodeFile="./pa10.xode", renderer=True, realtime=False, ip="127.0.0.1", port="21590", buf='16384'):
+    def __init__(self, xodeFile="./pa10.xode", renderer=True, realtime=True, ip="127.0.0.1", port="21590", buf='16384'):
         ODEEnvironment.__init__(self, renderer, realtime, ip, port, buf)
 
         # Load model file
-        #self.pert = array([0.0, 0.0, 0.0])
         self.loadXODE(xodeFile)
 
-        # Set the gravity to none
-        # TODO: This isn't modifying the gravity properly. See how this can be fixed
-        # NOTE: At the moment, gravity is set to 0.0 in .../ode/environments.py
-        #gravity = 0.0 #9.81
-        #self.world.setGravity((0.0, gravity, 0.0))
+        # Set the gravity to 0. The network can learn much easier without the
+        # force of gravity to counteract
+        gravity = 0.0
+        self.setGravity(gravity)
 
-        # Standard sensors and actuators
+        # Environment coefficient of friction
+        self.FricMu = 8.0
+
+        # Real-world time for each time step
+        self.dt = 0.005
+
+        # Number of steps time steps to simulate for each call to performAction()
+        self.stepsPerAction = 1
+
+        # Add PA10 sensors
         self.addSensor(sensors.JointSensor())
         self.addSensor(sensors.JointVelocitySensor())
+        self.addSensor(sensors.SpecificBodyPositionSensor(['pa10_t2'], 'tooltipPos'))
+
+        # Add PA10 actuators
         self.addActuator(actuators.JointActuator())
 
-        # Set number of actuators and sensors (observers)
+        # Reset number of actuators and sensors (observers)
         self.actLen = self.indim
         self.obsLen = self.outdim
 
-        #print 'NUM ACTUATORS: %d' % self.actLen
-        #print self.getActuatorNames()
-        #print 'NUM SENSORS: %d' % self.obsLen
-        #print self.getSensorNames()
-
-        # The torque values for the joints are normalized from the max torque in the env
-
-        # PA-10 realistic max joint torques
-        # s1 (rotate): 5.3
-        # s2 (pivot) : 5.3
-        # s3 (rotate): 2.0
-        # e1 (pivot) : 2.0
-        # e2 (rotate): 0.36
-        # w1 (pivot) : 0.36
-        # w2 (rotate): 0.36
-
-        self.torque_max = 5.3
-
-        torque_s1_rotate_norm = 5.3 / self.torque_max
-        torque_s2_pivot_norm = 5.3 / self.torque_max
-
-        torque_s3_rotate_norm = 2.0 / self.torque_max
-        torque_e1_pivot_norm = 2.0 / self.torque_max
-
-        torque_e2_rotate_norm = 0.36 / self.torque_max
-        torque_w1_pivot_norm = 0.36 / self.torque_max
-        torque_w2_rotate_norm = 0.36 / self.torque_max
-
-        # Set joint max torques
-        self.torqueList = array([
-            #torque_s1_rotate_norm,
-            torque_s2_pivot_norm,
-            torque_s3_rotate_norm,
-            torque_e1_pivot_norm,
-            torque_w1_pivot_norm,
-        ])
-
-        # NOTE: PyBrain accepts joint angles between -2pi and 2pi. Be sure
-        # to convert all values in degrees to radians.
-
-        # PA-10 realistic max/min joint angles [degrees]
-        # (Found in PA-10 manual)
-        # s1 (rotate): 177
-        # s2 (pivot) : 91
-        # s3 (rotate): 174
-        # e1 (pivot) : 137
-        # e2 (rotate): 255
-        # w1 (pivot) : 165
-        # w2 (rotate): 360
-
-        # Define joint max/min rotation angles
-        rotation = 360.0
-
-        # Shoulder joint max angles
-        s1_angle = 177.0 / rotation
-        s2_angle = 91.0 / rotation
-        s3_angle = 45.0 / rotation
-
-        # Elbow joint max angles
-        e1_angle = 137.0 / rotation
-        e2_angle = 255.0 / rotation
-
-        # Wrist joint max angles
-        w1_angle = 165.0 / rotation
-        w2_angle = 360.0 / rotation
-
-        # Set joint max/min rotation angle arrays (between -2pi and 2pi)
-        self.cHighList = array([
-            #s1_angle,
-            s2_angle,
-            s3_angle,
-            e1_angle,
-            w1_angle,
-        ]) * 2.0 * pi
-
-        self.cLowList = array([
-            #0.0,
-            0.0,
-            -s3_angle,
-            0.0,
-            0.0,
-        ]) * 2.0 * pi
-
-        self.stepsPerAction = 1
+        return
 
 
 if __name__ == '__main__' :
--- pa10/task.py
+++ pa10/task.py
@@ -1,18 +1,13 @@
 __author__ = 'Evan Sneath, evansneath@gmail.com'
 
 from pybrain.rl.environments import EpisodicTask
-from pybrain.rl.environments.ode.sensors import SpecificBodyPositionSensor
-from scipy import tanh, array, sqrt, absolute, pi
+
+import numpy as np
 
 
 class Pa10Task(EpisodicTask):
     def __init__(self, env):
         EpisodicTask.__init__(self, env)
-
-        # Take the defined max torque from the Pa10Environment as max power
-
-        # Power = Torque * Angular_Velocity
-        self.maxPower = self.env.torque_max * 2.0 * pi
 
         # Holds all rewards given in each episode
         self.reward_history = []
@@ -26,82 +21,173 @@ class Pa10Task(EpisodicTask):
         # Counts the task resets for incremental learning
         self.incLearn = 0
 
-        # Environment coefficient of friction
-        self.env.FricMu = 20.0 # 8.0
-
-        # Real-world time for each time step
-        self.env.dt = 0.005
-
-        # Add all actuators to the sensor limits
-        self.sensor_limits = []
+        # Sensor limit values can be used to normalize the sensors from
+        # (low, high) to (-1.0, 1.0) given the low and high values. We want to
+        # maintain all units by keeping the results unnormalized for now.
+        # Keeping the values of these lists at None skips the normalization.
+        self.sensor_limits = None
         self.actor_limits = None
 
-        # Set angle sensor limits
-        for i in range(self.env.actLen):
-            self.sensor_limits.append((self.env.cLowList[i], self.env.cHighList[i]))
+        # Create all attributes for joint limits
+        self.joint_max_angles = []
+        self.joint_min_angles = []
+        self.joint_max_velocities = []
+        self.joint_max_torques = []
 
-        # Set joint velocity sensor limits
-        for i in range(self.env.actLen):
-            self.sensor_limits.append((-2.0 * pi, 2.0 * pi))
+        # Call the setter function for the joint limit attributes
+        self.set_joint_angle_limits()
+        self.set_joint_velocity_limits()
+        self.set_joint_torque_limits()
 
-        # Add the PA-10 tooltip position sensor
-        self.env.addSensor(SpecificBodyPositionSensor(['pa10_t2'], 'tooltipPos'))
-
-        # Update the number of sensors
-        self.env.obsLen = self.env.outdim
-
-        old_act_num = self.env.actLen
-        self.env.actLen = self.env.indim
-
-        # Add the sensor limits of the tooltip sensor to the list (3 dimensions)
-        for i in range(3):
-            self.sensor_limits.append((-4, 4))
-        
         return
+
+
+    def set_joint_angle_limits(self):
+        # PA-10 realistic max/min joint angles [degrees]
+        # (Found in PA-10 manual)
+        # s1 (rotate): 177
+        # s2 (pivot) : 91
+        # s3 (rotate): 174
+        # e1 (pivot) : 137
+        # e2 (rotate): 255
+        # w1 (pivot) : 165
+        # w2 (rotate): 360
+
+        # Define joint maximum rotation angles
+        rotation = 360.0
+
+        # Shoulder joint max angles
+        s1_max_angle = 177.0 / rotation
+        s2_max_angle = 91.0 / rotation
+        s3_max_angle = 45.0 / rotation
+
+        # Elbow joint max angles
+        e1_max_angle = 137.0 / rotation
+        e2_max_angle = 180.0 / rotation
+
+        # Wrist joint max angles
+        w1_max_angle = 165.0 / rotation
+        w2_max_angle = 180.0 / rotation
+
+        # Set joint maximum rotation angles [rad]
+        self.joint_max_angles = np.array([
+            #s1_max_angle,
+            s2_max_angle,
+            s3_max_angle,
+            e1_max_angle,
+            #e2_max_angle,
+            w1_max_angle,
+            #w2_max_angle,
+        ]) * 2.0 * np.pi
+
+        # Define minimum rotation angles
+        s1_min_angle = -s1_max_angle
+        s2_min_angle = 0.0
+        s3_min_angle = -s3_max_angle
+
+        e1_min_angle = 0.0
+        e2_min_angle = -e2_max_angle
+
+        w1_min_angle = 0.0
+        w2_min_angle = -w2_max_angle
+
+        # Set joint mimumum rotation angles [rad]
+        self.joint_min_angles = np.array([
+            #s1_min_angle,
+            s2_min_angle,
+            s3_min_angle,
+            e1_min_angle,
+            #e2_min_angle,
+            w1_min_angle,
+            #w2_min_angle,
+        ]) * 2.0 * np.pi
+
+        return
+
+
+    def set_joint_velocity_limits(self):
+        # Define maximum joint velocities [rad/s]
+        #These values can be found in the PA10 user manual for each joint
+        s1_max_torque = 1.0
+        s2_max_torque = 1.0
+        s3_max_torque = 2.0
+        e1_max_torque = 2.0
+        e2_max_torque = 2.0 * np.pi
+        w1_max_torque = 2.0 * np.pi
+        w2_max_torque = 2.0 * np.pi
+
+        self.joint_max_velocities = np.array([
+            #s1_max_torque,
+            s2_max_torque,
+            s3_max_torque,
+            e1_max_torque,
+            #e2_max_torque,
+            w1_max_torque,
+            #w2_max_torque,
+        ])
+
+        return
+
+
+    def set_joint_torque_limits(self):
+        # PA-10 realistic max joint torques
+        # s1 (rotate): 5.3
+        # s2 (pivot) : 5.3
+        # s3 (rotate): 2.0
+        # e1 (pivot) : 2.0
+        # e2 (rotate): 0.36
+        # w1 (pivot) : 0.36
+        # w2 (rotate): 0.36
+
+        # The torque values for the joints are normalized from the max torque in the env
+        #self.torque_max = 5.3
+
+        torque_s1_rotate_norm = 5.3
+        torque_s2_pivot_norm = 5.3
+
+        torque_s3_rotate_norm = 2.0
+        torque_e1_pivot_norm = 2.0
+
+        torque_e2_rotate_norm = 0.36
+        torque_w1_pivot_norm = 0.36
+        torque_w2_rotate_norm = 0.36
+
+        # Set joint max torques
+        self.joint_max_torques = np.array([
+            #torque_s1_rotate_norm,
+            torque_s2_pivot_norm,
+            torque_s3_rotate_norm,
+            torque_e1_pivot_norm,
+            #torque_e2_rotate_norm,
+            torque_w1_pivot_norm,
+            #w2_max_torque,
+        ])
+
+        return
+
 
     def performAction(self, action):
         # Get joint angles and speeds
-        joints = array(self.env.getSensorByName('JointSensor'))
-        speeds = array(self.env.getSensorByName('JointVelocitySensor'))
 
-        #if self.count % self.epiLen == 500:
-        #    print 'JOINTS:', joints
-        #    print 'SPEEDS:', speeds
-        #    print 'ACTION:', action
-        #    print 'HIGH LIMIT:', self.env.cHighList, type(self.env.cHighList)
-        #    print 'LOW LIMIT:', self.env.cLowList, type(self.env.cLowList)
-        #    print 'TORQUES:', self.env.torqueList, type(self.env.torqueList)
-        #    print 'MAX TORQUE:', self.maxPower, type(self.maxPower)
-
-        # Convert all torques to anglular speeds
-        #act = ((action + 1.0) / 2.0 * (self.env.cHighList -
-        #          self.env.cLowList) + self.env.cLowList)
-        #action = (tanh((act - joints - 0.9 * speeds * self.env.torqueList) *
-        #          16.0) * self.maxPower * self.env.torqueList)
-
-        # Since the PA10 has joint velocity limits, these must be enforced in
-        # the output joint velocity action. Max velocity is determined to this
-        # point by Power * Torque, so in order to proportionally reduce the
-        # angular velocity, the current velocity must be multiplied by these
-        # scaling values
-
-        max_angle_velocities = array([
-            1.0 / (2.0 * pi), # S2 max = 1.0 [rad/s]
-            1.0 / pi,         # S3 max = 2.0 [rad/s]
-            1.0 / pi,         # E1 max = 2.0 [rad/s]
-            1.0,              # W1 max = 2.0 * pi [rad/s]
-        ])
+        # The joint angle values will be between -pi and pi in radians
+        joint_angles = np.asarray(self.env.getSensorByName('JointSensor'))
+        joint_velocities = np.asarray(self.env.getSensorByName('JointVelocitySensor'))
 
         # Change range from (-1.0, 1.0) to (0.0, 1.0)
         action = (action + 1.0) / 2.0
 
         # Scale the action between low and high joint angles
-        action = (action * (self.env.cHighList - self.env.cLowList) +
-                self.env.cLowList)
+        action = (action * (self.joint_max_angles - self.joint_min_angles) +
+                  self.joint_min_angles)
 
-        # Simple PID controller, convert torques to angular velocities
-        action = (tanh(action - joints - speeds * self.env.torqueList) *
-                  self.maxPower * self.env.torqueList * max_angle_velocities)
+        # Simple PID controller, convert angular velocities to torques to drive
+        # the joints. Since we want an output power, the action must be scaled
+        # by the max power of the system. Since power=torque*angular_velocity,
+        # We can multiply the maximum powers for each joint to find the desired
+        # output power.
+        action = (np.tanh(action - joint_angles - joint_velocities *
+                          self.joint_max_torques) *
+                  self.joint_max_torques * self.joint_max_velocities)
 
         # Carry out the action based on angular velocities
         EpisodicTask.performAction(self, action)
@@ -134,9 +220,9 @@ class Pa10MovementTask(Pa10Task):
         y_floor = -1.5
 
         # Set the current and old tooltip position attributes
-        self.tooltip_pos = array(self.env.getSensorByName('tooltipPos'))
-        self.old_tooltip_pos = array(self.env.getSensorByName('tooltipPos'))
-        self.tooltip_start = array(self.env.getSensorByName('tooltipPos'))
+        self.tooltip_pos = np.asarray(self.env.getSensorByName('tooltipPos'))
+        self.old_tooltip_pos = np.asarray(self.env.getSensorByName('tooltipPos'))
+        self.tooltip_start = np.asarray(self.env.getSensorByName('tooltipPos'))
 
         # Define velocity and acceleration values
         self.velocity = 0.0
@@ -145,7 +231,7 @@ class Pa10MovementTask(Pa10Task):
         self.acceleration = 0.0
 
         # Define the position of the target to hit
-        self.target_pos = array([0.6, y_floor+0.5, 0.2])
+        self.target_pos = np.array([0.6, y_floor+0.5, 0.2])
 
         # Initialize distance between the tooltip and target
         self.distance_to_target = self.calc_distance(
@@ -166,13 +252,13 @@ class Pa10MovementTask(Pa10Task):
             source: The start point.
             destination: The target point.
         """
-        return sqrt(((source - destination) ** 2).sum())
+        return np.sqrt(((source - destination) ** 2).sum())
 
     def getObservation(self):
         # Collect data about the world at each moment
 
         # Get the current tooltip location [m]
-        self.tooltip_pos = array(self.env.getSensorByName('tooltipPos'))
+        self.tooltip_pos = np.asarray(self.env.getSensorByName('tooltipPos'))
 
         # Calculate the straightline distance from the arm to the target point [m]
         self.distance_to_target = self.calc_distance(
@@ -190,7 +276,7 @@ class Pa10MovementTask(Pa10Task):
         self.velocity = pos_difference / self.env.dt
 
         # Calculate the acceleration of this moment [m/s^2]
-        self.acceleration = absolute(self.velocity - self.old_velocity)
+        self.acceleration = np.absolute(self.velocity - self.old_velocity)
 
         # Set the values of this moment as "old" values
         self.old_velocity = self.velocity
