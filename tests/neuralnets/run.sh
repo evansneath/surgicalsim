8e84e8071b084d3f9ea303c8a5b2966c6d2d85ee
evansneath <evansneath@gmail.com>
2013-09-25 14:24:20 -0400

    Modified absolute path of neural net test run script

--- tests/neuralnets/run.sh
+++ tests/neuralnets/run.sh
@@ -2,7 +2,7 @@
 
 # A shell script to run the render engine and environment
 
-SURGICAL_SIM_DIR=~/Workspace/surgical-sim/network-test
+SURGICAL_SIM_DIR=~/Workspace/surgicalsim/tests/neuralnets
 
 # Start the render engine (viewer)
 $SURGICAL_SIM_DIR/viewer.sh &
